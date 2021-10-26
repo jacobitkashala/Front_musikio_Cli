73cf766e4cf2e97a9781bb48a6b021fb1817103e
jacobitkashala <jacobitkashala@gmail.com>
2021-10-26 10:34:41 +0200

    styled backroud and image

--- src/Components/McLogo/index.js
+++ src/Components/McLogo/index.js
@@ -1,33 +1,33 @@
-import React from 'react';
-import styled from 'styled-components/native';
-import { Colors } from '../../Constants';
+// import React from 'react';
+// import styled from 'styled-components/native';
+// import { Colors } from '../../Constants';
 
-const McLogo = () => {
-  return (
-    <Container>
-      <Circle>
-        <Text>M</Text>
-      </Circle>
-    </Container>
-  );
-};
+// const McLogo = () => {
+//   return (
+//     <Container>
+//       <Circle>
+//         <Text>M</Text>
+//       </Circle>
+//     </Container>
+//   );
+// };
 
-const Container = styled.View`
-  width:${props => props.size || 78}px;
-  height:${props => props.size || 78}px;
-`;
-const Circle = styled.View`
-  background-color: ${Colors.colorBgrLogo};
-  border-radius:${70 / 2}px;;
-  width:${props => props.circle || 70}px;
-  height:${props => props.circle || 70}px;
-  align-items: center;
-  justify-content: center;
-`;
+// const Container = styled.View`
+//   width:${props => props.size || 78}px;
+//   height:${props => props.size || 78}px;
+// `;
+// const Circle = styled.View`
+//   background-color: ${Colors.colorBgrLogo};
+//   border-radius:${70 / 2}px;;
+//   width:${props => props.circle || 70}px;
+//   height:${props => props.circle || 70}px;
+//   align-items: center;
+//   justify-content: center;
+// `;
 
-const Text = styled.Text`
-  font-size: 40px;
-  color: ${Colors.black};
-`;
-// font-family:${Fonts.type.fontLogo}
-export default McLogo;
+// const Text = styled.Text`
+//   font-size: 40px;
+//   color: ${Colors.black};
+// `;
+// // font-family:${Fonts.type.fontLogo}
+// export default McLogo;
--- src/Screens/Onboarding/index.js
+++ src/Screens/Onboarding/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {StatusBar, StyleSheet, Text, Image} from 'react-native';
 import styled from 'styled-components/native';
-
 import {Images, Colors, Metrics} from '../../Constants';
 // import {McLogo} from '../../Components';
 
@@ -9,7 +8,7 @@ const Onboarding = ({loadAssetsAsync}) => {
   return (
     <Container>
       <StatusBar barStyle="light-content" />
-      <Image source={Images.logo} />
+      <Image source={Images.logo} style={styles.logo} />
       <Image
         source={Images.cover}
         style={styles.image}
@@ -25,10 +24,16 @@ const Onboarding = ({loadAssetsAsync}) => {
   );
 };
 const Container = styled.SafeAreaView`
+  padding-top: ${Metrics.s5}px;
   flex: 1;
   background-color: ${Colors.black};
+  justify-content: flex-start;
 `;
 const styles = StyleSheet.create({
+  logo: {
+    width: Metrics.s100,
+    height: Metrics.s100,
+  },
   text: {
     color: Colors.colorText,
     fontSize: Metrics.s20,
@@ -37,6 +42,7 @@ const styles = StyleSheet.create({
   image: {
     width: Metrics.width,
     height: Metrics.width,
+    padding: Metrics.s5,
     backgroundColor: 'gray',
     // cursor: 'pointer'
   },
