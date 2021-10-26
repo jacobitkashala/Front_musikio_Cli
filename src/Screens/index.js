631367f410fc79c60c5faa0524343ccb26c96957
jacobitkashala <jacobitkashala@gmail.com>
2021-10-26 09:17:59 +0200

    add: creation de components

---
+++ src/Screens/index.js
@@ -0,0 +1,12 @@
+// import Home from "./Home";
+// import Profile from "./Profile";
+import Onboarding from './Onboarding';
+// import Player from "./Player";
+// import Library from "./Library";
+// import Users from "./Users";
+// import Favories from "./Favories";
+// import Podcast from "./Podcast";
+// import Audio from "./Audio";
+
+// export { Audio, Users, Library, Podcast, Favories, Home, Onboarding, Player, Profile };
+export {Onboarding};
